ab37599dbb496f134358549e79807e3da9f295fd
DevPatils <dev.patil1006@gmail.com>
2024-09-01 11:40:56 +0530

    CORS added

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const router = require('./Routes/index');
+const cors = require('cors');
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
